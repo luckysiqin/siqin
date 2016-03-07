b3d19dab72113ee86d08093e0f57bd62d718a720
luckysiqin <a20131105804@Mac28.local>
2016-03-07 21:19:49 +0800

    drdrdr
    huh

---
+++ main.swift
@@ -0,0 +1,18 @@
+//
+//  main.swift
+//  siqin
+//
+//  Created by 20131105804 on 16/3/3.
+//  Copyright © 2016年 20131105804. All rights reserved.
+//
+
+import Foundation
+var a:Int,b:Int,c:Int
+a=100
+b=200
+c=a+b
+for var i in(1...10){
+  print("\(i)")
+}
+print("\(a)+\(b)=\(c)")
+
